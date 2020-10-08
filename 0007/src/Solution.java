db78f1552f3527ddf67707a765f4084627e8240e
syp1997 <1050743451@qq.com>
2020-10-08 20:19:44 +0800

    0007 added

---
+++ 0007/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int y = 0;
+        int a;
+
+        while (x != 0){
+            a = x % 10;
+            if(y > (Integer.MAX_VALUE) / 10 || y < (Integer.MIN_VALUE ) / 10){
+                return 0;
+            }
+            y = y*10 + a;
+            x = x/10;
+        }
+        return y;
+    }
+}
